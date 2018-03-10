dfca540801a0fbf7d9c51c45fc6260bc738e5b3b
Abdelouali Benkheil <a.benkheil@gmail.com>
2018-03-10 01:25:23 +0100

    Create Announcements.php

---
+++ languages/nl_be/Announcements.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Announcements dutch translation.
+ *
+ * @copyright YetiForce Sp. z o.o
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ */
+$languageStrings = [
+	'Announcements' => 'Aankondigingen',
+	'SINGLE_Announcements' => 'Aankondiging',
+	//BLOCKS
+	'LBL_ANNOUNCEMENTS_INFORMATION' => 'Basis informatie',
+	//FIELDS
+	'LBL_SUBJECT' => 'Onderwerp',
+	'LBL_NUMBER' => 'Nummer',
+	'FL_STATUS' => 'Status',
+	'FL_INTERVAL' => 'Herinneringstijd',
+	'FL_IS_MANDATORY' => 'Verplicht om te accepteren',
+	// Picklist values
+	'PLL_DRAFT' => 'Concept',
+	'PLL_FOR_ACCEPTANCE' => 'Ter goedkeuring',
+	'PLL_PUBLISHED' => 'Gepubliceerd',
+	'PLL_ARCHIVES' => 'Gearchiveerd',
+	// Alert
+	'LBL_REMIND_ME_LATER' => 'Herinner me later',
+	'LBL_MARK_AS_READ' => 'Markeer als gelezen',
+	// Others
+	'LBL_ACCEPT_ANNOUNCEMENT' => 'Was geaccepteerd?',
+	'LBL_DATE' => 'Lees verder',
+];
