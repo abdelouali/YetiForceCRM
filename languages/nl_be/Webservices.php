b59acb6b7241aa50570270382cadb6fa20fffd18
Abdelouali Benkheil <a.benkheil@gmail.com>
2018-03-10 01:27:25 +0100

    Create Webservices.php

---
+++ languages/nl_be/Webservices.php
@@ -0,0 +1,17 @@
+<?php
+/* * *******************************************************************************
+ * * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): Abdelouali Benkheil
+ * ********************************************************************************** */
+$languageStrings = [
+	'Webservices' => 'Webservices',
+	'LBL_CHANGE_PASSWORD_FAILURE' => 'Wachtwoord wijzigen mislukt',
+	'LBL_DATABASE_QUERY_ERROR' => 'Databasefout tijdens het uitvoeren van de gevraagde bewerking',
+	'LBL_INVALID_OLD_PASSWORD' => 'Ongeldige waarde gegeven voor oud wachtwoord.',
+	'LBL_NEW_PASSWORD_MISMATCH' => 'Nieuw wachtwoord en wachtwoord bevestigen komen niet overeen',
+];
